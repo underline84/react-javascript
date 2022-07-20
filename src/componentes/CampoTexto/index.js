300a6d4e0c0fcc240b50013daeecc4cb3958de09
underline84 <evanfatu@hotmail.com>
2022-07-20 15:24:33 -0300

    first commit

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+import './CampoTexto.css'
+
+export const CampoTexto = (props) => {
+    return (
+        <div className="campo-texto">
+            <label>{props.label}</label>
+            <input placeholder={props.placeholder}/>
+        </div>
+    )
+}
